9ef5921eae6fd8c7a30e2d6461b4c7f419acf89d
TeslaCN <root496879118@me.com>
2019-08-13 12:30:19 +0800

    Update Bash script run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,14 +4,19 @@ EXE_FILENAME='haveyouordered'
 
 echo 'Starting have-you-order server.'
 
+cd scripts
 cd ..
-go build cmd/orderserver/orderserver.go
+
+pwd
 
 PID=$(pgrep -f ${EXE_FILENAME})
 echo "Previous executing: ${PID}"
 [[ ${PID} == '' ]] || kill "${PID}"
 
-go build -o ${EXE_FILENAME}
+echo 'Building...'
+
+pwd
+go build -o ${EXE_FILENAME} cmd/orderserver/orderserver.go
 nohup ./${EXE_FILENAME} &
 
 echo 'have-you-ordered Started.'
